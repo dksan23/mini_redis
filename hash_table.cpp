65ab565c4a5eddd0462f1b9a5c3e8a27407905b0
dksan23 <47032393+dksan23@users.noreply.github.com>
2023-07-01 04:42:13 +0000

    redis_hash_table_impl

--- hash_table.cpp
+++ hash_table.cpp
@@ -1,145 +1,141 @@
-#include "hash_table.h"
+#include <assert.h>
+#include <stdlib.h>
+#include "hashtable.h"
 
-static void h_init(htab *htab, size_t n)
-{
-    assert(n > 0 && ((n-1) & n == 0));
-    htab->tab = (hnode**) calloc(sizeof(hnode *), n);
+
+// n must be a power of 2
+static void h_init(HTab *htab, size_t n) {
+    assert(n > 0 && ((n - 1) & n) == 0);
+    htab->tab = (HNode **)calloc(sizeof(HNode *), n);
+    htab->mask = n - 1;
     htab->size = 0;
-    htab->mask = n-1;
 }
 
-static void h_insert(htab* htab, hnode* node)
-{
-    if(!htab->tab)
-    {
-        return;
-    }
+// hashtable insertion
+static void h_insert(HTab *htab, HNode *node) {
     size_t pos = node->hcode & htab->mask;
-    hnode* next = htab->tab[pos];
+    HNode *next = htab->tab[pos];
     node->next = next;
     htab->tab[pos] = node;
     htab->size++;
 }
+
 // hashtable look up subroutine.
 // Pay attention to the return value. It returns the address of
 // the parent pointer that owns the target node,
 // which can be used to delete the target node.
-static hnode **h_lookup(htab *htab, hnode* key, bool (*cmp) (hnode* , hnode* ))
+static HNode **h_lookup(
+    HTab *htab, HNode *key, bool (*cmp)(HNode *, HNode *))
 {
-    if(!htab->tab)
-    {
-        return nullptr;
+    if (!htab->tab) {
+        return NULL;
     }
-    size_t pos = htab->mask & key->hcode;
-    hnode ** from = &htab->tab[pos];
-    while(*from)
-    {
-        if(cmp(*from, key))
-        {
+
+    size_t pos = key->hcode & htab->mask;
+    HNode **from = &htab->tab[pos];
+    while (*from) {
+        if (cmp(*from, key)) {
             return from;
         }
+        from = &(*from)->next;
     }
-
-    return nullptr;
+    return NULL;
 }
 
-static hnode* h_detach(htab *htab, hnode **from)
-{
-    if(!htab->tab)
-    {
-        return nullptr;
-    }
-    hnode *node = *from;
+// remove a node from the chain
+static HNode *h_detach(HTab *htab, HNode **from) {
+    HNode *node = *from;
     *from = (*from)->next;
     htab->size--;
     return node;
 }
 
-static void hmap_resize(HMap* hmap)
-{
-    if(hmap->ht1.tab == nullptr)
-    {
+const size_t k_resizing_work = 128;
+
+static void hm_help_resizing(HMap *hmap) {
+    if (hmap->ht2.tab == NULL) {
         return;
     }
 
-    uint16_t nwork = 0;
-    while(nwork < RESIZING_BATCH && hmap->ht1.size > 0)
-    {
-        hnode** from = &hmap->ht1.tab[hmap->resizing_pos];
-        if(!*from)
-        {
+    size_t nwork = 0;
+    while (nwork < k_resizing_work && hmap->ht2.size > 0) {
+        // scan for nodes from ht2 and move them to ht1
+        HNode **from = &hmap->ht2.tab[hmap->resizing_pos];
+        if (!*from) {
             hmap->resizing_pos++;
             continue;
         }
 
-        h_insert(&hmap->ht2, h_detach(&hmap->ht1, from));
+        h_insert(&hmap->ht1, h_detach(&hmap->ht2, from));
+        nwork++;
     }
 
-    if(hmap->ht1.size == 0)
-    {
-        free(hmap->ht1.tab);
-        hmap->ht1 = htab{};
+    if (hmap->ht2.size == 0) {
+        // done
+        free(hmap->ht2.tab);
+        hmap->ht2 = HTab{};
     }
 }
 
-hnode* hm_lookup(HMap* hmap, hnode* node, bool (*cmp) (hnode* , hnode* ))
-{
-    if(!hmap->ht1.tab && !hmap->ht2.tab)
-    {
-        return nullptr;
-    }
-
-    hnode **from = h_lookup(&hmap->ht1, node, cmp);
-    if(!from)
-    {
-        from = h_lookup(&hmap->ht2, node, cmp);
-    }
-
-    return !from ? nullptr : *from;
-}
-
-static void hm_insert(HMap *hmap, hnode* node)
-{
-    if(hmap->ht1.tab == nullptr)
-    {
-        h_init(&hmap->ht1, 4);
-    }
-
-    h_insert(&hmap->ht1, node);
-
-    if(!hmap->ht2.tab)
-    {
-        size_t load_factor = hmap->ht1.size / (hmap->ht1.mask + 1);
-        if(load_factor >= MAX_LOAD_FACTOR)
-        {
-            hmap_start_resize(hmap);
-        }
-    }
-
-    hmap_resize(hmap);
-}
-
-static void hmap_start_resize(HMap* hmap)
-{
-    assert(hmap->ht2.tab == nullptr);
+static void hm_start_resizing(HMap *hmap) {
+    assert(hmap->ht2.tab == NULL);
+    // create a bigger hashtable and swap them
     hmap->ht2 = hmap->ht1;
-    h_init(&hmap->ht2, (hmap->ht1.mask + 1) * 2);
+    h_init(&hmap->ht1, (hmap->ht1.mask + 1) * 2);
     hmap->resizing_pos = 0;
 }
 
-static hnode* hm_pop(HMap* hmap, hnode* node, bool (*cmp) (hnode* , hnode* ))
+HNode *hm_lookup(
+    HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode *))
 {
-    hnode** from = h_lookup(&hmap->ht1, node, cmp);
-    if(from)
-    {
+    hm_help_resizing(hmap);
+    HNode **from = h_lookup(&hmap->ht1, key, cmp);
+    if (!from) {
+        from = h_lookup(&hmap->ht2, key, cmp);
+    }
+    return from ? *from : NULL;
+}
+
+const size_t k_max_load_factor = 8;
+
+void hm_insert(HMap *hmap, HNode *node) {
+    if (!hmap->ht1.tab) {
+        h_init(&hmap->ht1, 4);
+    }
+    h_insert(&hmap->ht1, node);
+
+    if (!hmap->ht2.tab) {
+        // check whether we need to resize
+        size_t load_factor = hmap->ht1.size / (hmap->ht1.mask + 1);
+        if (load_factor >= k_max_load_factor) {
+            hm_start_resizing(hmap);
+        }
+    }
+    hm_help_resizing(hmap);
+}
+
+HNode *hm_pop(
+    HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode *))
+{
+    hm_help_resizing(hmap);
+    HNode **from = h_lookup(&hmap->ht1, key, cmp);
+    if (from) {
         return h_detach(&hmap->ht1, from);
     }
-
-    from = h_lookup(&hmap->ht2, node, cmp);
-    if(from)
-    {
+    from = h_lookup(&hmap->ht2, key, cmp);
+    if (from) {
         return h_detach(&hmap->ht2, from);
     }
+    return NULL;
+}
 
-    return from ? *from : nullptr;
+size_t hm_size(HMap *hmap) {
+    return hmap->ht1.size + hmap->ht2.size;
+}
+
+void hm_destroy(HMap *hmap) {
+    assert(hmap->ht1.size + hmap->ht2.size == 0);
+    free(hmap->ht1.tab);
+    free(hmap->ht2.tab);
+    *hmap = HMap{};
 }
--- hash_table.h
+++ hash_table.h
@@ -1,44 +1,30 @@
-#include<stdint.h>
-#include<stdlib.h>
-#include<assert.h>
-#include<string>
-
-#define RESIZING_BATCH 128
-#define MAX_LOAD_FACTOR 8
-
-#define container_of(ptr, type, member) ({ \
-const typeof( ((type *)0)->member ) *__mptr = (ptr); \
-(type *)( (char *)__mptr - offsetof(type, member) );})
+#include <stddef.h>
+#include <stdint.h>
 
 
-typedef struct hnode
-{
-    hnode* next = nullptr;
+// hashtable node, should be embedded into the payload
+struct HNode {
+    HNode *next = NULL;
     uint64_t hcode = 0;
-}Hnode;
+};
 
-typedef struct htab
-{
-    hnode** tab = nullptr;
-    uint64_t mask = 0;
-    uint64_t size = 0;
-}Htab;
+// a simple fixed-sized hashtable
+struct HTab {
+    HNode **tab = NULL;
+    size_t mask = 0;
+    size_t size = 0;
+};
 
-/* Resizing HashMap*/  //should a eviciton strategy based approach
-
-struct HMap
-{
-    htab ht1;
-    htab ht2;
+// the real hashtable interface.
+// it uses 2 hashtables for progressive resizing.
+struct HMap {
+    HTab ht1;
+    HTab ht2;
     size_t resizing_pos = 0;
 };
 
-struct Entry {
-    struct hnode node;
-    std::string key;
-    std::string val;
-};
-
-static struct {
-    HMap db;
-} g_data;
+HNode *hm_lookup(HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode *));
+void hm_insert(HMap *hmap, HNode *node);
+HNode *hm_pop(HMap *hmap, HNode *key, bool (*cmp)(HNode *, HNode *));
+size_t hm_size(HMap *hmap);
+void hm_destroy(HMap *hmap);
